1ae6871e47f1b2619122fca10dc7eab0ff595ff0
Luan Luz <luanbrunoalvesluz@gmail.com>
2019-09-10 16:13:08 -0300

    ajax p2

--- lesson17/ajax/server.js
+++ lesson17/ajax/server.js
@@ -24,5 +24,37 @@ app.use(bodyParser.json());
 
 app.get('/teste',(req, res) => res.send('OK'));
 
+// definindo o storage
+const storage = multer.diskStorage(
+    {
+        // definindo o local onde será salvo o arquivo
+        destination: function(req, file, callback)
+        {
+            callback(null, './uploads');
+        },
 
-app.listen(3000, () => console.log('Executando na porta 8080'));
+        // definindo o nome final do arquivo
+        filename: function(req, file, callback)
+        {
+            callback(null, `${Date.now()}_${file.originalname}`);
+        }
+    }
+);
+
+const upload = multer({storage}).single('arquivo');
+
+// quando o arquivo chegar, ele vai ter esse tratamento
+app.post('/upload', (req, res) => {
+    upload(req, res, err =>
+        {
+            if(err)
+            {
+                // caso tenha tido erro
+                return res.end('Ocorreu um erro ao enviar arquivo.')
+            }
+            // caso não tenha erro
+            res.end('Concluído com sucesso.')
+        });
+});
+
+app.listen(3000, () => console.log('Executando na porta 3000'));
